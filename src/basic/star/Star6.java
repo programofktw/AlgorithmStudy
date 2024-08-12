b4793ed3f40d3b711ddca1c54eb21e31e9dfe1e2
programofktw <thkim000317@naver.com>
2024-08-12 15:41:30 +0900

    Star6 add

--- src/basic/star/Star6.java
+++ src/basic/star/Star6.java
@@ -1,30 +1,29 @@
 package basic.star;
 
-
-import java.util.Map;
 import java.util.Scanner;
 /*
-   *
-  ***
- *****
 *******
+ *****
+  ***
+   *
  */
-public class Star5 implements StarInterface{
+public class Star6 implements StarInterface{
+
     @Override
     public void StarPrint() {
         Scanner scan = new Scanner(System.in);
-        int H = scan.nextInt();
-        int W = 2*H-1;
 
+        int H = scan.nextInt();
+        int W = 2 * H-1;
         int mid = W/2;
-        for(int i = 0; i<H;i++){
-            for(int j = 0; j< W; j++){
+
+        for(int i = H-1; i>=0;i--){
+            for(int j = 0 ; j<W;j++){
                 if(mid-i<=j&&mid+i>=j) System.out.print("*");
                 else{
                     System.out.print(" ");
                 }
             }
-            if(i==H-1) continue;
             System.out.println();
         }
     }
