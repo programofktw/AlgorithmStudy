f36bdd4ccae77af89e5c584082a5dfa599646521
programofktw <thkim000317@naver.com>
2024-08-25 16:37:36 +0900

    solved 15740

---
+++ src/baekjoon/bronze/BackJoon15740.java
@@ -0,0 +1,16 @@
+package baekjoon.bronze;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BackJoon15740 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        BigInteger A = scan.nextBigInteger();
+
+        BigInteger B = scan.nextBigInteger();
+
+        System.out.println(A.add(B));
+    }
+}
