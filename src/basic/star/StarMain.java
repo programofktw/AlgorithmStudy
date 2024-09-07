38fce3fb14df10e11029c635f83f6dc45c6c273e
programofktw <thkim000317@naver.com>
2024-09-07 14:01:57 +0900

    Star8 add

--- src/basic/star/StarMain.java
+++ src/basic/star/StarMain.java
@@ -2,7 +2,7 @@ package basic.star;
 
 public class StarMain {
     public static void main(String[] args){
-        StarInterface star = new Star7();
+        StarInterface star = new Star8();
 
 
         star.StarPrint();
