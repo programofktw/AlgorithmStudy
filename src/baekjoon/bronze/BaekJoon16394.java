4681b22a42cebd045861e563996631ad9919ee4a
programofktw <thkim000317@naver.com>
2024-09-12 22:02:00 +0900

    solved 16394

---
+++ src/baekjoon/bronze/BaekJoon16394.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class BaekJoon16394 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int now = scan.nextInt();
+
+        System.out.println(now - 1946);
+    }
+}
