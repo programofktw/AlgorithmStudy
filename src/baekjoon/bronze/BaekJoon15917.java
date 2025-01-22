55573554ad541f76a36782ba058a9a58e7cafb62
programofktw <thkim000317@naver.com>
2025-01-22 16:02:53 +0900

    modify 15917 replace condition

--- src/baekjoon/bronze/BaekJoon15917.java
+++ src/baekjoon/bronze/BaekJoon15917.java
@@ -1,24 +1,24 @@
 package baekjoon.bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class BaekJoon15917 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int Q = Integer.parseInt(br.readLine());
 
-        for(int i = 0 ; i<Q;i++){
+        for (int i = 0; i < Q; i++) {
             long a = Long.parseLong(br.readLine());
-
-            if((a&(a*-1))==a){
-                sb.append(1).append("\n");
-            }else{
-                sb.append(0).append("\n");
+            if ((a & (a - 1)) == 0) {
+                bw.write("1\n");
+            } else {
+                bw.write("0\n");
             }
         }
-        System.out.print(sb);
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
