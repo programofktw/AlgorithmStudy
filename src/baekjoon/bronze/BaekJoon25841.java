3dc1e688f6f73d700cda9688de65dd478e7e7408
programofktw <thkim000317@naver.com>
2025-01-23 02:47:59 +0900

    25841 int type solved

--- src/baekjoon/bronze/BaekJoon25841.java
+++ src/baekjoon/bronze/BaekJoon25841.java
@@ -3,22 +3,29 @@ package baekjoon.bronze;
 import java.util.Scanner;
 
 public class BaekJoon25841 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         int f = scan.nextInt();
         int s = scan.nextInt();
+        int findNum = scan.nextInt();
 
-        String findNum = scan.next();
         int count = 0;
-        for(int i=f ; i<=s;i++){
-            String string = ""+i;
-
-            for(int j=0;j<string.length();j++){
-                if(string.charAt(j)==findNum.charAt(0)) count++;
-            }
+        for (int i = f; i <= s; i++) {
+            count += countOccurrences(i, findNum);
         }
 
         System.out.print(count);
+    }
+
+    private static int countOccurrences(int number, int digit) {
+        int count = 0;
+        while (number > 0) {
+            if (number % 10 == digit) {
+                count++;
+            }
+            number /= 10;
+        }
+        return count;
     }
 }
