dd1328aad080802950040a797798183797d26465
programofktw <thkim000317@naver.com>
2025-01-24 02:45:28 +0900

    26876 used BufferedReader & Syso

--- src/baekjoon/silver/BaekJoon26876.java
+++ src/baekjoon/silver/BaekJoon26876.java
@@ -1,18 +1,22 @@
 package baekjoon.silver;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BaekJoon26876 {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(scan.nextLine(),":");
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine(),":");
 
         int FH = Integer.parseInt(st.nextToken().trim());
         int FM = Integer.parseInt(st.nextToken().trim());
 
-        st = new StringTokenizer(scan.next(),":");
+        st = new StringTokenizer(br.readLine(),":");
 
         int SH = Integer.parseInt(st.nextToken());
         int SM = Integer.parseInt(st.nextToken());
