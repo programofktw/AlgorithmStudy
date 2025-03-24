fec787a0d4b15323127cea251b605203e0412b4e
programofktw <thkim000317@naver.com>
2025-03-24 12:32:19 +0900

    SortInterface make

---
+++ src/basic/sort/Sort.java
@@ -0,0 +1,16 @@
+package basic.sort;
+
+public interface Sort {
+
+    void sort(int[] array);
+
+
+    default void swap(int[] array, int i, int j){
+        int temp = array[i];
+
+        array[i] = array[j];
+
+        array[j] = temp;
+    }
+
+}
