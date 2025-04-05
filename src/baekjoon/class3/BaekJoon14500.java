a9322b132b9b6605d7066c4de820e27ffd608e97
programofktw <thkim000317@naver.com>
2025-04-05 14:47:51 +0900

    BaekJoon 14500 not done

---
+++ src/baekjoon/class3/BaekJoon14500.java
@@ -0,0 +1,20 @@
+package baekjoon.class3;
+
+
+/**
+ * 테트로미노
+ * N X M 크기의 정사각형이 존재
+    * 각각의 칸에는 정수가 하나 쓰여 있음
+ * 5가지 형태의 테트로미노가 존재
+ * N X M 크기의 정사각형에 테트로미노를 하나 두었을 때 정수들의 합이 가장 큰 경우 찾기
+ * 어떻게?
+ * 가장 쉬운 방법 1. 모든 테트로미노 형태로 모든 경우를 탐색해보기
+ *
+ */
+public class BaekJoon14500 {
+
+
+    public static void main(String[] args){
+
+    }
+}
