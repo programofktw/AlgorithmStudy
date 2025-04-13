a9da91a99d7c9c356da6a9d83e423221bdc9f6ae
programofktw <thkim000317@naver.com>
2025-04-13 15:09:15 +0900

    BaekJoon14500 done

--- src/baekjoon/class3/BaekJoon14500.java
+++ src/baekjoon/class3/BaekJoon14500.java
@@ -11,10 +11,79 @@ package baekjoon.class3;
  * 가장 쉬운 방법 1. 모든 테트로미노 형태로 모든 경우를 탐색해보기
  *
  */
+// [백준] 14500. 테트로미노 (Java)
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BaekJoon14500 {
 
+    static int max = Integer.MIN_VALUE;
+    static int[][] arr;
+    static boolean[][] visit;
+    static int n;
+    static int m;
 
-    public static void main(String[] args){
+    // 상하좌우
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
 
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n][m];
+        visit = new boolean[n][m];
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                visit[i][j] = true;
+                solve(i,j,arr[i][j],1);
+                visit[i][j] = false;
+            }
+        }
+
+        System.out.println(max);
+    }
+
+    static void solve(int row, int col, int sum, int count) {
+
+        if(count == 4) {
+            max = Math.max(max, sum);
+            return;
+        }
+
+        for(int i = 0; i < 4; i++) {
+            int curRow = row + dx[i];
+            int curCol = col + dy[i];
+
+            if(curRow < 0 || curRow >= n || curCol < 0 || curCol >= m) {
+                continue;
+            }
+
+            if(!visit[curRow][curCol]) {
+
+                if(count == 2) {
+                    visit[curRow][curCol] = true;
+                    solve(row, col, sum + arr[curRow][curCol], count + 1);
+                    visit[curRow][curCol] = false;
+                }
+
+                visit[curRow][curCol] = true;
+                solve(curRow, curCol, sum + arr[curRow][curCol], count + 1);
+                visit[curRow][curCol] = false;
+            }
+        }
     }
 }
