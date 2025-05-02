c7bc9ef059af57c26495edd37366c0357dd95e73
programofktw <thkim000317@naver.com>
2025-05-03 00:18:36 +0900

    solved 13909

---
+++ src/baekjoon/silver/BaekJoon13909.java
@@ -0,0 +1,18 @@
+package baekjoon.silver;
+
+
+import java.util.Scanner;
+
+/*
+창문닫기
+ */
+public class BaekJoon13909 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+
+        int N = scan.nextInt();
+
+        System.out.print((int)Math.sqrt(N));
+    }
+}
