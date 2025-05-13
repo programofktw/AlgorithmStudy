d4eea1fdcdad335f2e52d06d4f0b5365a1c161e4
programofktw <thkim000317@naver.com>
2025-05-13 22:32:53 +0900

    시도 2 - dp를 활용하기 - 시간복잡도 O(n^2) 해결

--- src/baekjoon/class4/BaekJoon11053.java
+++ src/baekjoon/class4/BaekJoon11053.java
@@ -3,7 +3,6 @@ package baekjoon.class4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 /*
@@ -13,48 +12,29 @@ import java.util.StringTokenizer;
 
 => 시간 초과 발생.
 => 시도 1. 입력의 시간을 줄여보기
+=> 시도 2. dp 이용.
  */
 public class BaekJoon11053 {
 
-    static int[] array;
-
-    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N  = Integer.parseInt(br.readLine());
-        array = new int[N];
 
-        StringTokenizer input = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(br.readLine());
+        int[] array = new int[N];
+        int[] dp = new int[N];  // dp[i] = i를 마지막 원소로 하는 LIS의 길이
 
-        int i = 0;
-        while(input.hasMoreTokens()){
-            array[i++] = Integer.parseInt(input.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
         }
 
-
-        int contain = backTraking(1,array[0],1);
-        int notContain = backTraking(1,0,0);
-
-        System.out.print(Math.max(contain,notContain));
-
-    }
-
-    //now : 현재 탐색하고자 하는 인덱스
-    //last : 가장 마지막에 추가된 수(가장 큰 수)
-    //count : 몇개의 수열이 포함되었는지
-    private static int backTraking(int now,int last, int count){
-       //만약 가장 마지막 인덱스까지 끝 마쳤다면(index는 0~N-1 까지) 결과를 반환
-        if(now==N) return count;
-
-        else{
-            //포함하지 않았을 때
-            int notContain = backTraking(now+1,last,count);
-            //값의 범위가 1보다 크기 때문에 max에 걸려지도록 0으로 기본 값 설정
-            int contain = 0;
-            //현재 탐색 중인 인덱스의 수가 가장 마지막에 추가된 수보다 큰 경우 포함.
-            if(array[now]>last) contain = backTraking(now+1,array[now],count+1);
-
-            return Math.max(contain,notContain);
-        }
+       for(int i = 0 ;i<N;i++){
+           dp[i]=1;
+           for(int j=0;j<i;j++){
+               if(array[j] < array[i]) {
+                   dp[i] = Math.max(dp[i], dp[j] + 1);
+               }
+           }
+       }
     }
 }
