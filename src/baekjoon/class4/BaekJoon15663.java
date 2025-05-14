7b44b5513e79bc533f7631818744ecabae9e2f5f
programofktw <thkim000317@naver.com>
2025-05-14 17:01:09 +0900

    [15663] : HashMap 의 put과 get 메소드로 인한 불필요한 시간 복잡도 해결을 위해 backTraking 방식을 사용

--- src/baekjoon/class4/BaekJoon15663.java
+++ src/baekjoon/class4/BaekJoon15663.java
@@ -11,12 +11,13 @@ import java.util.StringTokenizer;
 public class BaekJoon15663 {
 
     static int[] array;
-    static boolean[] visited;
 
     static Map<Integer, Integer> input= new HashMap<>();
     static int N;
     static int M;
+    static boolean[] visited;
 
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -24,9 +25,8 @@ public class BaekJoon15663 {
 
         N  = Integer.parseInt(tokens[0]);
         M  = Integer.parseInt(tokens[1]);
-
-        array = new int[N];
         visited = new boolean[N];
+        array = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int i = 0;
@@ -36,20 +36,26 @@ public class BaekJoon15663 {
 
         //정렬
         Arrays.sort(array);
+        //dfs 풀이 부분
+/*        for(int num : array){
+//            if(input.containsKey(num)){
+//                input.put(num,input.get(num)+1);
+//            }else{
+//                input.put(num,1);
+//            }
+//        }
+//
+//        for(int num : input.keySet()){
+//            input.put(num,input.get(num)-1);
+//            dfs(1,num+"");
+//            input.put(num,input.get(num)+1);
+//        }*/
 
-        for(int num : array){
-            if(input.containsKey(num)){
-                input.put(num,input.get(num)+1);
-            }else{
-                input.put(num,1);
-            }
-        }
 
-        for(int num : input.keySet()){
-            input.put(num,input.get(num)-1);
-            dfs(1,num+"");
-            input.put(num,input.get(num)+1);
-        }
+
+        backTraking(0,"");
+
+        System.out.print(sb);
     }
 
     static void dfs(int depth, String now){
@@ -59,11 +65,34 @@ public class BaekJoon15663 {
             for(int num : input.keySet()){
                 if(input.get(num)>0){
                     input.put(num,input.get(num)-1);
-                    dfs(depth+1,now+" "+num);
+                    dfs(depth,now+" "+num);
                     input.put(num,input.get(num)+1);
                 }
             }
         }
 
+    }
+
+    static void backTraking(int depth, String now){
+        if(depth==M){
+            sb.append(now).append("\n");
+        }
+        else{
+            int lastUsed = -1;
+            for(int i = 0; i<N;i++){
+                if(lastUsed!=array[i]&&!visited[i]){
+                    String next;
+                    if(depth==0){
+                        next = ""+array[i];
+                    }else{
+                        next = now+" "+array[i];
+                    }
+                    visited[i] = true;
+                    backTraking(depth+1,next);
+                    visited[i] = false;
+                    lastUsed = array[i];
+                }
+            }
+        }
     }
 }
