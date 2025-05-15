f314dac929d0bb93c19921776de1eb18a5a4e514
programofktw <thkim000317@naver.com>
2025-05-15 17:21:47 +0900

    [16953] 테스트 케이스에서는 성공했지만 baekjoon은 실패

---
+++ src/baekjoon/class4/BaekJoon16953.java
@@ -0,0 +1,54 @@
+package baekjoon.class4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon16953 {
+
+    static int B;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+        solved(A);
+    }
+
+    public static void solved(int now){
+
+        int result;
+
+        int result1 = backTraking(now*10+1,1);
+        int result2 = backTraking(now*2,1);
+
+        if(result1 == -1&&result2==-1) result= -1;
+        else if(result1==-1)result = result2;
+        else if(result2==-1) result = result1;
+        else result = Math.min(result2,result1);
+
+        System.out.print((result!=-1)?result+1:-1);
+
+    }
+
+    public static int backTraking(int now,int depth){
+        if(now > B){
+            return -1;
+        }
+        if(now == B) return depth;
+        int result;
+
+        int result1 = backTraking(now*10+1,depth+1);
+        int result2 = backTraking(now*2,depth+1);
+
+        if(result1 == -1&&result2==-1) result= -1;
+        else if(result1==-1)result = result2;
+        else if(result2==-1) result = result1;
+        else result = Math.min(result2,result1);
+
+        return result;
+    }
+}
