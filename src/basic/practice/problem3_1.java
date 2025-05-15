e7d782e0f62e93bb3e83dad8ae63496b85e0cbf4
programofktw <thkim000317@naver.com>
2025-05-15 12:18:26 +0900

    [basic] : TestCase를 입력받고 해당 횟수 만큼 반복하도록 수정

--- src/basic/practice/problem3_1.java
+++ src/basic/practice/problem3_1.java
@@ -8,41 +8,35 @@ public class problem3_1 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
-        N = scan.nextInt();
-
-        int[] array = new int[3];
+        int TestCase = Integer.parseInt(scan.nextLine());
 
 
-        array[0] = 1;
-        array[1] = 1;
-        array[2] = 2;
-        for(int i =3;i<=N;i++){
-            int temp = array[0] + array[1] + array[2];
-            array[0] = array[1];
-            array[1] = array[2];
-            array[2] = temp;
+        for(int i = 0 ; i<TestCase;i++){
+            N = Integer.parseInt(scan.nextLine());
+
+
+
+            int[] array = new int[3];
+
+
+            array[0] = 1;
+            array[1] = 1;
+            array[2] = 2;
+            if(N<3){
+                System.out.println(array[N]);
+                continue;
+            }
+
+            for(int j =3;j<=N;j++){
+                int temp = array[0] + array[1] + array[2];
+                array[0] = array[1];
+                array[1] = array[2];
+                array[2] = temp;
+            }
+
+
+            System.out.println(array[2]);
         }
 
-//        for(int i = 1;i<=3;i++){
-//           if(N>=i) count +=dfs(i,""+i);
-//        }
-
-
-        System.out.print(array[2]);
-    }
-
-
-    public static int dfs(int now, String hap){
-        int count = 0;
-        if(now>N) return 0;
-        else if(now ==N){
-            System.out.println(hap);
-            return 1;
-        }
-        for(int i =1;i<=3;i++){
-            if(N>=now+i) count +=dfs(now+i,hap +" + "+ i);
-        }
-
-        return count;
     }
 }
