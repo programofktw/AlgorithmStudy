604f32cc02b4721790e386bce68035b842aafc17
programofktw <thkim000317@naver.com>
2025-05-17 14:02:41 +0900

    [11660] 풀이 1 실패 : 이유 - 행과 열 방식의 헷갈림

--- src/baekjoon/class4/BaekJoon11660.java
+++ src/baekjoon/class4/BaekJoon11660.java
@@ -1,8 +1,64 @@
 package baekjoon.class4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BaekJoon11660 {
 
-    public static void main(String[] args){
+    static int[][] dp;
+
+    static int[][] field;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+
+        int N = Integer.parseInt(st.nextToken());
+
+        int M = Integer.parseInt(st.nextToken());
+
+        field = new int[N][N];
+        dp = new int[N][N];
+        for(int i = 0 ; i<N;i++){
+            int j = 0;
+            st = new StringTokenizer(br.readLine());
+            int sum =0;
+            while(st.hasMoreTokens()){
+                field[i][j] = Integer.parseInt(st.nextToken());
+                sum +=field[i][j];
+
+                dp[i][j++] = sum;
+            }
+        }
+
+        for(int i = 0; i<M;i++){
+            StringTokenizer testCase = new StringTokenizer(br.readLine());
+
+            int result = 0;
+            int startX = Integer.parseInt(testCase.nextToken())-1;
+
+            int startY = Integer.parseInt(testCase.nextToken())-1;
+
+            int endX = Integer.parseInt(testCase.nextToken())-1;
+
+            int endY = Integer.parseInt(testCase.nextToken())-1;
+
+            for(int y = startY; y<=endY;y++){
+                if(startX-1<0){
+                    result += dp[y][endX];
+                }else{
+                    result += dp[y][endX] - dp[y][startX-1];
+                }
+            }
+            System.out.println(result);
+
+        }
+
 
     }
 }
