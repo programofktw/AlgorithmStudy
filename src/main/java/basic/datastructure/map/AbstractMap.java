50d236f203c48a9fc27d20cadbce959ec6d8f52e
programofktw <thkim000317@naver.com>
2025-07-08 13:38:27 +0900

    [MAP] : ListMap 구현

--- src/main/java/basic/datastructure/map/AbstractMap.java
+++ src/main/java/basic/datastructure/map/AbstractMap.java
@@ -2,15 +2,8 @@ package basic.datastructure.map;
 
 public abstract class AbstractMap<K,V> implements Map<K,V>{
 
-    private int size = 0;
 
-    public int size(){
-        return size;
-
-    }
-
-    public boolean isEmpty(){
-        if(size==0) return true;
-        return false;
+    protected void checkNullKey(K key){
+        if(key==null) throw new NullPointerException();
     }
 }
