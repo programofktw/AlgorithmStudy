4d80b64d4ac42f8bc58d848b26378b793d00ea43
programofktw <thkim000317@naver.com>
2025-07-09 12:12:07 +0900

    [HASHMAP] : hash map 구현

---
+++ src/main/java/basic/datastructure/map/HashMap.java
@@ -0,0 +1,38 @@
+package basic.datastructure.map;
+
+public class HashMap<K,V> extends AbstractMap<K,V> {
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
