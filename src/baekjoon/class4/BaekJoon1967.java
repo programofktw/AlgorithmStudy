614543238a97e28ed8205295e4bcc623d543d09a
programofktw <thkim000317@naver.com>
2025-08-15 16:44:36 +0900

    [FEAT] 주석 추가

--- src/baekjoon/class4/BaekJoon1967.java
+++ src/baekjoon/class4/BaekJoon1967.java
@@ -26,22 +26,28 @@ public class BaekJoon1967 {
 
     //Node간의 연결 표시
     //num이 key인 Node에 연결된 Node들의 집합.
-    static Map<Integer, List<Node>> graph = new HashMap<>();
+    static Map<Integer, List<Node>> graph;
 
     public static void main(String[] args) throws IOException {
         //node의 개수
         int n = Integer.parseInt(br.readLine());
 
+        //node 에 연결된 다른 Node들 초기화
         init(n);
 
+        //입력을 받아서 각 노드별 연결 정도 확인
         input(n);
 
+        //결과 탐색 및 반환
         int result = searchResult(n);
 
+        //결과 출력
         System.out.print(result);
     }
 
+    //그래프 초기화 시키기 작업
     static void init(int n){
+        graph = new HashMap<>();
         for(int i = 1;i<=n;i++){
             graph.put(i, new ArrayList<>());
         }
@@ -64,6 +70,8 @@ public class BaekJoon1967 {
     }
 
     //리프 노드에서 다른 노드들까지의 거리 중 최댓값을 찾아서 반환.
+    //이때 작업하는 방식은 루트 노드로 부터 가장 거리가 먼 Node를 찾고
+    //그 이후 해당 먼 Node에서 다른 모든 노드들의 거리를 찾아 반환.
     static int searchResult(int n){
         return di(farNode());
     }
