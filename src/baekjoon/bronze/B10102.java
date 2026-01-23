0c5c41d4a041fe98504d9fb7b852d19c2fd0ac84
programofktw <thkim000317@naver.com>
2026-01-23 23:36:12 +0900

    baekjoon 10102 refactor

--- src/baekjoon/bronze/B10102.java
+++ src/baekjoon/bronze/B10102.java
@@ -3,6 +3,7 @@ package baekjoon.bronze;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class B10102 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,24 +11,44 @@ public class B10102 {
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
+//        br.readLine();
+//
+//        String st = br.readLine();
+//
+//        int total = st.length();
+//
+//        st = st.replace("A","");
+//
+//        int BNum = st.length();
+//
+//        double mid = (double)total/2;
+//
+//        if(mid>BNum){
+//            sb.append("A");
+//        }else if(mid == BNum){
+//            sb.append("Tie");
+//        }else{
+//            sb.append("B");
+//        }
+//
+//        System.out.print(sb);
         br.readLine();
 
-        String st = br.readLine();
+        String s = br.readLine();
 
-        int total = st.length();
+        int count=0;
 
-        st = st.replace("A","");
+        for(int i =0;i<s.length();i++){
+            char vote = s.charAt(i);
 
-        int BNum = st.length();
 
-        double mid = (double)total/2;
+            count += (vote=='A')?1:-1;
+        }
 
-        if(mid>BNum){
-            sb.append("A");
-        }else if(mid == BNum){
+        if(count==0){
             sb.append("Tie");
         }else{
-            sb.append("B");
+            sb.append((count>0)?"A":"B");
         }
 
         System.out.print(sb);
