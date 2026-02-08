e8f97c4bd207a57b250da2e36902536a29a330bb
programofktw <thkim000317@naver.com>
2026-02-08 23:24:43 +0900

    B15439 solved

---
+++ src/baekjoon/bronze/B15439.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class B15439 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.print(n*(n-1));
+    }
+
+}
