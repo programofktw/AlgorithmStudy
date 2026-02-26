9fc6fde827ff8410dde11547e271ca89e42183b1
programofktw <thkim000317@naver.com>
2026-02-26 23:25:15 +0900

    refactor 10833

--- src/baekjoon/bronze/B10833.java
+++ src/baekjoon/bronze/B10833.java
@@ -12,43 +12,21 @@ public class B10833 {
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
 
-        int[] students = new int[n];
 
-        int[] apples = new int[n];
 
-        boolean[] flag = new boolean[n];
+        int result = 0;
+
 
         for(int i = 0; i < n; i++) {
             String[] input = br.readLine().split(" ");
 
-            students[i] = Integer.parseInt(input[0]);
+            int student = Integer.parseInt(input[0]);
 
-            apples[i] = Integer.parseInt(input[1]);
+            int apple = Integer.parseInt(input[1]);
+
+            result += apple % student;
         }
 
-
-        int i = 0;
-
-        int result = 0;
-
-        while(true){
-            boolean done = true;
-            for(int j = 0; j < n; j++){
-                if(flag[j]){
-                    continue;
-                }
-                if(students[j]*(i+1)>apples[j]){
-                    flag[j] = true;
-                    result += apples[j]-(students[j]*i);
-                }else{
-                    done = false;
-                }
-            }
-            if(done){
-                break;
-            }
-            i++;
-        }
 
         sb.append(result);
 
