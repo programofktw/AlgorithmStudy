429a000f009ef9674fcf8896883d9368de4541c0
programofktw <thkim000317@naver.com>
2026-04-08 22:49:20 +0900

    [bj] 27434 factorial 3 solved

---
+++ src/baekjoon/bronze/B27434.java
@@ -0,0 +1,31 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class B27434 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        System.out.println(factorial(1, N));
+    }
+
+    public static BigInteger factorial(int a, int n) {
+        BigInteger num = BigInteger.valueOf(a);
+        if (a < n) {
+            int b = (a + n) / 2;
+            num = factorial(a, b).multiply(factorial(b + 1, n));
+        }
+        return num;
+    }
+}
